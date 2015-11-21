e4878bb3274fc60dd59ed4a560bcfa77466b7d1a
grannycookies <16batesa@gmail.com>
2015-11-21 14:45:13 +0000

    267

--- dev.php
+++ dev.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app="">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
 		
@@ -9,14 +9,24 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 		<script src="/js/jquery.js"></script>
 		
-		<!-- materializecss.com  -->
-		<link rel="stylesheet" href="/css/main.css">
-		<script src="/js/materialize.js"></script>
-		
 		<!-- sprint files -->
 		<link rel="stylesheet" href="/editor/editor.css">
 		<script src="/editor/js/editor.js"></script>
 	</head>
 	
-	<body ng-controller="controller"></body>
+	<body ng-app="editor">
+		<main class="row" ng-controller="controller">
+			<div class="col s12 m6 l8">
+			
+			</div>
+
+			<div class="col s12 m6 l8">
+
+			</div>
+		</main>
+		
+		<footer>
+			Created with &hearts; by the <a href="/about/team/">Sprint Team</a>
+		</footer>
+	</body>
 </html>
