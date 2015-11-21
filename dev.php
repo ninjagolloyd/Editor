9f78e357302356d419837567c14f254e10b60d41
grannycookies <16batesa@gmail.com>
2015-11-21 14:20:13 +0000

    262

--- dev.php
+++ dev.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" ng-app>
 	<head>
+		<meta charset="utf-8">
+		
 		<title>Sprint Editor</title>
 		
+		<link rel="stylesheet" href="/css/main.css">
 		<link rel="stylesheet" href="editor.css">
+		
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 	</head>
+	
+	<body>
+		<p>
+			{{'hello' + ' world!'}}
+		</p>
+	</body>
 </html>
